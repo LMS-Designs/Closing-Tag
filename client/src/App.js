558ed652ab13ab0e7057335f4e9896658bde6330
Unknown <nannerfox@me.com>
2019-04-29 17:19:51 -0500

    Re-stage init with Create-React-App 3.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import LMS from './LMS.png';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <i className="App-logo fas fa-code fa-10x"></i>
-          <p>
-          <h2><i className="fas fa-chevron-left"></i><b>/CLOSING TAG</b><i className="fas fa-chevron-right"></i></h2>
-          </p>
-          <h4>LMS Productions</h4>
-          <img id="LMStack" src={LMS} alt="Lick My Stack"/>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
