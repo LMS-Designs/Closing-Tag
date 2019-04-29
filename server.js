edef7356657c4f8b1927376508f9cfcb268ed602
Alan Wilson <nannerfox@me.com>
2019-04-29 18:57:14 -0500

    MySQL setup

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const morgan = require('morgan');
-const db = mongoose.connection;
 const app = express();
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('ClosingTag', 'webconnection', '@Closingtagisdabomb101', {
+    host: 'closing-tag.cypm94q3l3td.us-east-2.rds.amazonaws.com',
+    dialect: 'mysql'
+  });
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 // Development mode port
 const port = process.env.PORT || 5000;
 app.listen(port)
@@ -29,21 +38,6 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
-
-mongoose.connect('mongodb://nodelogin:lickmystack1@ds219191.mlab.com:19191/closing-tag', function(err) {
-  if (err) {
-    console.err(err);
-  } else {
-    console.log('Connected');
-  }
-});
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('DB connected!');
-});
-mongoose.set('bufferCommands', false);
-mongoose.set('debug', true);
 
 //routes.gohere
 
